52f2f5c899b91fd87a1ba6de4989bf2cbc52fdc7
Arnav Gupta <arnavgupta-11408@bvniketan.onmicrosoft.com>
2023-11-09 13:19:22 +0530

    Add mysql2 package and denque dependency

--- index.js
+++ index.js
@@ -5,87 +5,92 @@ const port = process.env.PORT || process.env.port || 3000;
 const http = require("http");
 const server = http.createServer(app);
 
+const dotenv = require("dotenv");
+dotenv.config();
+
 const io = require("socket.io")(server);
 
 const { QuickDB, MySQLDriver } = require("quick.db");
 
-const mysqlDriver = new MySQLDriver({
-  host: "sql12.freesqldatabase.com",
-  user: "sql12660470",
-  password: "Z7bnEFB8ZL",
-  database: "sql12660470",
-});
-mysqlDriver.connect();
-db = new QuickDB({ driver: mysqlDriver });
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-const fs = require("fs");
-
-const session = require("express-session");
-
-var sqlite = require("better-sqlite3");
-var SqliteStore = require("better-sqlite3-session-store")(session);
-var seshdb = new sqlite("sessions.db");
-
-app.use(
-  session({
-    store: new SqliteStore({
-      client: seshdb,
-      expired: {
-        clear: true,
-        intervalMs: 900000, //ms = 15min
-      },
-    }),
-    secret: "euphindia on the TOP!!",
-    resave: true,
-    saveUninitialized: false,
-    cookie: { secure: false },
-  })
-);
-
-app.engine("ejs", require("express-art-template"));
-app.set("view engine", "ejs");
-
-app.use(express.static("public"));
-
-fs.readdir("./routes", (err, files) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  files.forEach((file) => {
-    require("./routes/" + file)(app, io, db);
-  });
-});
-
-fs.readdir("./backend", (err, files) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  files.forEach((file) => {
-    require("./backend/" + file)(app, io, db);
-  });
-});
-
 (async () => {
-  if (!(await db.get("products"))) {
-    await db.set("products", []);
-  }
-  if (!(await db.get("coupons"))) {
-    await db.set("coupons", []);
-  }
-  if (!(await db.get("affiliate"))) {
-    await db.set("affiliate", []);
-  }
-  if (!(await db.get("checkouts"))) {
-    await db.set("checkouts", []);
-  }
-})();
+  const mysqlDriver = new MySQLDriver({
+    host: process.env.DB_HOST,
+    user: "sql12660470",
+    password: process.env.DB_PASSWORD,
+    database: "sql12660470",
+  });
+  await mysqlDriver.connect();
+  var db = new QuickDB({ driver: mysqlDriver });
 
-server.listen(port, () => {
-  console.log("Listening on port " + port);
-});
+  const bodyParser = require("body-parser");
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+
+  const fs = require("fs");
+
+  const session = require("express-session");
+
+  var sqlite = require("better-sqlite3");
+  var SqliteStore = require("better-sqlite3-session-store")(session);
+  var seshdb = new sqlite("sessions.db");
+
+  app.use(
+    session({
+      store: new SqliteStore({
+        client: seshdb,
+        expired: {
+          clear: true,
+          intervalMs: 900000, //ms = 15min
+        },
+      }),
+      secret: "euphindia on the TOP!!",
+      resave: true,
+      saveUninitialized: false,
+      cookie: { secure: false },
+    })
+  );
+
+  app.engine("ejs", require("express-art-template"));
+  app.set("view engine", "ejs");
+
+  app.use(express.static("public"));
+
+  fs.readdir("./routes", (err, files) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    files.forEach((file) => {
+      require("./routes/" + file)(app, io, db);
+    });
+  });
+
+  fs.readdir("./backend", (err, files) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    files.forEach((file) => {
+      require("./backend/" + file)(app, io, db);
+    });
+  });
+
+  (async () => {
+    if (!(await db.get("products"))) {
+      await db.set("products", []);
+    }
+    if (!(await db.get("coupons"))) {
+      await db.set("coupons", []);
+    }
+    if (!(await db.get("affiliate"))) {
+      await db.set("affiliate", []);
+    }
+    if (!(await db.get("checkouts"))) {
+      await db.set("checkouts", []);
+    }
+  })();
+
+  server.listen(port, () => {
+    console.log("Listening on port " + port);
+  });
+})();
